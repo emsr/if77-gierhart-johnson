0ce90b493f2b04e0fa8316e7744a1d8ba43d634d
Ed Smith-Rowland <esmithrowland@gmail.com>
2022-06-13 13:58:23 -0400

    Addd the other toys to build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,9 @@
 gfortran -g -fno-align-commons -o atoa src/*.for 2> warn.txt
 
 ./atoa < atoa.in > atoa.out
+
+gfortran -g -fno-align-commons -o run_raytrace_old run_raytrace_old.for src/amsc.for src/asorp.for
+
+gfortran -g -fno-align-commons -o run_raytrace run_raytrace.f95
+
+g++ -g -DQUIET=0 -o run_raytrace_new run_raytrace_new.cpp
